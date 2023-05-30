4ae51efbd8b2132f1c4ed766a42967f172192c42
usher <usher@DESKTOP-NST8773>
2023-05-31 00:40:01 +0400

    Add project files.

---
+++ Library/Repositories/Repo.cs
@@ -0,0 +1,104 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Library.Repositories
+{
+    public class Repo
+    {
+        SqlConnection conn;
+        string cs = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+        DataSet set = new DataSet();
+        public Repo()
+        {
+            //conn = new SqlConnection();
+            using (conn = new SqlConnection())
+            {
+                var da = new SqlDataAdapter();
+                conn.ConnectionString = cs;
+                conn.Open();
+
+                SqlCommand command = new SqlCommand("SELECT * FROM Authors", conn);
+
+                da.SelectCommand = command;
+                da.Fill(set, "AuthorsSet");
+            }
+        }
+
+        public DataSet GetAll()
+        {
+            return set;
+        }
+
+        public void AddAuthor(int id, string firstName, string lastName)
+        {
+            using (conn = new SqlConnection())
+            {
+                var command = new SqlCommand("INSERT INTO Authors(Id,FirstName,LastName) VALUES(@id,@firstName,@lastName)", conn);
+                conn.ConnectionString = cs;
+
+                command.Parameters.Add(new SqlParameter
+                {
+                    DbType = DbType.Int32,
+                    ParameterName = "@id",
+                    Value = id
+                });
+
+                command.Parameters.Add(new SqlParameter
+                {
+                    SqlDbType = SqlDbType.NVarChar,
+                    ParameterName = "@firstName",
+                    Value = firstName
+                });
+
+                command.Parameters.Add(new SqlParameter
+                {
+                    SqlDbType = SqlDbType.NVarChar,
+                    ParameterName = "@lastName",
+                    Value = lastName
+                });
+
+                var da = new SqlDataAdapter();
+                da.InsertCommand = command;
+                da.Update(set, "AuthorsSet");
+                set.Clear();
+
+                da = new SqlDataAdapter("SELECT * FROM Authors", conn);
+
+                da.Fill(set, "AuthorsSet");
+            }
+        }
+
+        public void DeleteAuthor(int id)
+        {
+            using (conn = new SqlConnection())
+            {
+                var command = new SqlCommand("DELETE FROM Authors WHERE Id=@id;", conn);
+                conn.ConnectionString = cs;
+
+                command.Parameters.Add(new SqlParameter
+                {
+                    DbType = DbType.Int32,
+                    ParameterName = "@id",
+                    Value = id
+                });
+
+                var da = new SqlDataAdapter();
+                da.DeleteCommand = command;
+                da.Update(set, "AuthorsSet");
+                set.Clear();
+
+                da = new SqlDataAdapter("SELECT * FROM Authors", conn);
+                da.Fill(set, "AuthorsSet");
+            }
+        }
+    }
+}
---
+++ Library/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,75 @@
+﻿using Library.Commands;
+using Library.Models;
+using Library.Repositories;
+using System.Collections.ObjectModel;
+using System.Data;
+
+namespace Library.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+
+        public RelayCommand CheckAllCommand { get; set; }
+        public RelayCommand SelectionChanged { get; set; }
+        public RelayCommand RefreshCommand { get; set; }
+        public RelayCommand AddAuthorCommand { get; set; }
+        public RelayCommand DeleteCommand { get; set; }
+
+        public Repo AuthorsRepo { get; set; }
+        public DataSet AuthorSet { get; set; } = new DataSet();
+
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged(); }
+        }
+
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; OnPropertyChanged(); }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; OnPropertyChanged(); }
+        }
+
+        private int ID;
+
+        public int SelectedId
+        {
+            get { return ID; }
+            set { ID = value; OnPropertyChanged(); }
+        }
+
+        public MainWindowViewModel()
+        {
+            AuthorsRepo = new Repo();
+
+            AuthorSet = AuthorsRepo.GetAll();
+
+            AddAuthorCommand = new RelayCommand((obj) =>
+            {
+                AuthorsRepo.AddAuthor(Id, FirstName, LastName);
+            });
+
+            SelectionChanged = new RelayCommand((obj) =>
+            {
+                    AuthorsRepo.DeleteAuthor(SelectedId);
+            });
+
+            RefreshCommand = new RelayCommand((obj) =>
+            {
+                AuthorSet = AuthorsRepo.GetAll();
+            });
+        }
+    }
+}
